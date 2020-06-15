69aa39c084369a7364dc6d6e12fe2f181dc35b01
BiaVilalta <beatrizvilaltajimenez@gmail.com>
2020-06-15 19:59:58 -0300

    Initial Commit

--- struct1/main.c
+++ struct1/main.c
@@ -8,8 +8,74 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+int main() {
+  
+    int hour[3], i;
+    
+    printf("Type what time your apointment is (hour, minute, second): \n");
+    for (i = 0; i < 3; i++){
+        if (i > 0){
+            printf(":");
+        }
+        fseek(stdin, 0, SEEK_END);
+        scanf(" %d", &hour[i]);
+    }
+    
+    int date[3];
+    
+    printf("Type the date of your apointment (day, month, year): \n");
+    for (i = 0; i < 3; i++){
+        if (i > 0){
+            printf("/");
+        }
+        fseek(stdin, 0, SEEK_END);
+        scanf(" %d", &date[i]);
+       
+    }
+    
+    char description[60];
+    
+    printf("Add a description: ");
+    fseek(stdin, 0, SEEK_END);
+    gets(description);
+    
+    
+    printf("Hour: ");
+    
+    for (i = 0; i < 3; i++){
+        
+        if (i > 0) {
+            printf(":");
+        }
+        printf(" %d", hour[i]);
+    }
+    
+    printf("\n");
+    
+    printf("Date: ");
+    
+    for (i = 0; i < 3; i++) {
+        
+        if (i > 0) {
+            printf("/");
+        }
+        printf(" %d", date[i]);
+    }
+    printf("\n");
+    printf("Description: %s\n", description);
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }

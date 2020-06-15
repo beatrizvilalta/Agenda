c7dbbb1cafc7df8200c391b90ffff96a42f86989
BiaVilalta <beatrizvilaltajimenez@gmail.com>
2020-06-15 18:43:08 -0300

    Initial Commit

---
+++ struct1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  struct1
+//
+//  Created by Beatriz Vilalta on 15/06/20.
+//  Copyright © 2020 Beatriz Vilalta. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
